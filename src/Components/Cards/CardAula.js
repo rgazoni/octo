70435b79144d9a1f756be6494f49dabf2d0c9911
EduardoFujiki <eduardofmaeda@hotmail.com>
2023-06-05 17:24:28 -0300

    card da aula finalizado

--- src/Components/Cards/CardAula.js
+++ src/Components/Cards/CardAula.js
@@ -2,37 +2,31 @@ import React from 'react'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
-
-
 import 'bootstrap/dist/css/bootstrap.css';
-
-
 
 export default function CardAula() {
   return (
     <>
-     <Card className="w-50">
-        <Card.Header>
-            <h2 >Cálcuclo</h2>
-            <Button className="position-absolute top-0 end-0 " variant="primary">Adicionar Arquivo</Button>
+    <div className="d-flex justify-content-around">
+     <Card className="w-50 p-3 shadow p-3 mb-5 bg-body-tertiary">
+        <Card.Header className="d-flex justify-content-between">
+        <span class="align-middle"><h2>Nome da aula</h2></span>
+        <Button variant="dark">Adicionar Arquivo</Button>
         </Card.Header>
         
         <Card.Body>
             {/* <Card.Title>Special title treatment</Card.Title> */}
             <Card.Text>
                 <ListGroup as="ol" numbered>
-                  <ListGroup.Item as="li">Limite</ListGroup.Item>
-                  <ListGroup.Item as="li">Derivada</ListGroup.Item>
-                  <ListGroup.Item as="li">Integral</ListGroup.Item>
+                  <ListGroup.Item as="li">Assunto 1</ListGroup.Item>
+                  <ListGroup.Item as="li">Assunto 2</ListGroup.Item>
+                  <ListGroup.Item as="li">Assunto 3</ListGroup.Item>
                 </ListGroup>
             </Card.Text>
         
         </Card.Body>
     </Card>
-
-
-    </>
-    
-    
+    </div>
+   </> 
   )
 }
