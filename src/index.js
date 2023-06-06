1f4ca43dc4ab446457c27592fbce38c653f4b6fe
Lucas Koda <lukaskoda50@gmail.com>
2023-06-06 12:48:31 -0300

    screen files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ModalDisciplina from "./Components/Modal/ModalDisciplina";
-/*import ModalArquivos from "./Components/Modal/ModalArquivos";*/
+/*import ModalDisciplina from "./Components/Modal/ModalDisciplina";*/
+import ModalArquivos from './Components/Modal/ModalArquivos';
+
 ReactDOM.render(
   <React.StrictMode>
-    <ModalDisciplina />
-    {/* <ModalArquivos/> */}
+    {/*<ModalDisciplina />*/}
+     <ModalArquivos/> 
   </React.StrictMode>,
   document.getElementById('root')
 );
