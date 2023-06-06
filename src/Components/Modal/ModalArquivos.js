17cbf33ac98e23e339d842d51a858dcbf0842d65
Lucas Koda <lukaskoda50@gmail.com>
2023-06-06 13:46:40 -0300

    button save

--- src/Components/Modal/ModalArquivos.js
+++ src/Components/Modal/ModalArquivos.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function ModalArquivos() {
+  const [isModalOpen, setModalOpen] = useState(false);
   const [fileInputs, setFileInputs] = useState([0]);
 
   const adicionarCampoArquivo = () => {
@@ -13,53 +14,60 @@ function ModalArquivos() {
     setFileInputs(prevInputs => prevInputs.filter((_, i) => i !== index));
   };
 
+  const handleSalvar = () => {
+    // Lógica para salvar os arquivos
+    setModalOpen(false);
+    setFileInputs([0]); // Limpar os campos de arquivo
+  };
+
   return (
     <div>
       {/* Botão para abrir o modal */}
       <button
         type="button"
         className="btn btn-primary modal-button"
-        data-bs-toggle="modal"
-        data-bs-target="#myModal"
+        onClick={() => setModalOpen(true)}
       >
         Modal
       </button>
 
       {/* Modal */}
-      <div className="modal fade" id="myModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Deseja Adicionar Arquivos</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
-            </div>
-            <div className="modal-body">
-              <div id="arquivosContainer">
-                {fileInputs.map((index) => (
-                  <div className="mb-3" key={index}>
-                    <label htmlFor={`arquivosAula${index}`} className="form-label">Arquivos:</label>
-                    <div className="input-group">
-                      <input type="file" className="form-control" name={`arquivosAula${index}[]`} multiple />
-                      <button
-                        type="button"
-                        className="btn btn-danger"
-                        onClick={() => removerArquivo(index)}
-                      >
-                        X
-                      </button>
-                    </div>
-                  </div>
-                ))}
+      {isModalOpen && (
+        <div className="modal fade show" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Deseja Adicionar Arquivos</h5>
+                <button type="button" className="btn-close" onClick={() => setModalOpen(false)}></button>
               </div>
-              <button type="button" className="btn btn-secondary mt-2" onClick={adicionarCampoArquivo}>Adicionar Arquivo</button>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-              <button type="button" className="btn btn-primary">Salvar</button>
+              <div className="modal-body">
+                <div id="arquivosContainer">
+                  {fileInputs.map((index) => (
+                    <div className="mb-3" key={index}>
+                      <label htmlFor={`arquivosAula${index}`} className="form-label">Arquivos:</label>
+                      <div className="input-group">
+                        <input type="file" className="form-control" name={`arquivosAula${index}[]`} multiple />
+                        <button
+                          type="button"
+                          className="btn btn-danger"
+                          onClick={() => removerArquivo(index)}
+                        >
+                          X
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <button type="button" className="btn btn-secondary mt-2" onClick={adicionarCampoArquivo}>Adicionar Arquivo</button>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={() => setModalOpen(false)}>Fechar</button>
+                <button type="button" className="btn btn-primary" onClick={handleSalvar}>Salvar</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/Components/Modal/ModalDisciplina.js
+++ src/Components/Modal/ModalDisciplina.js
@@ -1,45 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function ModalDisciplina() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleSalvar = () => {
+    // Lógica para salvar os dados do formulário
+    setModalOpen(false);
+  };
+
   return (
     <div>
       {/* Botão para abrir o modal */}
       <button
         type="button"
         className="btn btn-primary modal-button"
-        data-bs-toggle="modal"
-        data-bs-target="#myModal"
+        onClick={() => setModalOpen(true)}
       >
         Modal
       </button>
 
       {/* Modal */}
-      <div className="modal fade" id="myModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">Deseja Adicionar Aula</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
-            </div>
-            <div className="modal-body">
-              <div className="mb-3">
-                <label htmlFor="nomeAula" className="form-label">Nome:</label>
-                <input type="text" className="form-control" id="nomeAula" />
+      {isModalOpen && (
+        <div className="modal fade show" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Deseja Adicionar Aula</h5>
+                <button type="button" className="btn-close" onClick={() => setModalOpen(false)}></button>
               </div>
-              <div className="mb-3">
-                <label htmlFor="descricaoAula" className="form-label">Descrição:</label>
-                <textarea className="form-control" id="descricaoAula" rows="3"></textarea>
+              <div className="modal-body">
+                <div className="mb-3">
+                  <label htmlFor="nomeAula" className="form-label">Nome:</label>
+                  <input type="text" className="form-control" id="nomeAula" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="descricaoAula" className="form-label">Descrição:</label>
+                  <textarea className="form-control" id="descricaoAula" rows="3"></textarea>
+                </div>
               </div>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-              <button type="button" className="btn btn-primary">Salvar</button>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={() => setModalOpen(false)}>Fechar</button>
+                <button type="button" className="btn btn-primary" onClick={handleSalvar}>Salvar</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
