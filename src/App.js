4efe2596d86c23e8048980c1ab35d559cf103728
Ramon Gazoni <ramongazonil@gmail.com>
2023-06-09 19:44:40 -0300

    hot fix card aula

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import Sidebar from "./Components/Sidebar/Sidebar";
 import { Col, Container, Row } from 'react-bootstrap'
+import CardAula from "./Components/Cards/CardAula";
 
 function App() {
   return (
     <>
-    <Container fluid>
+      <Container fluid>
         <Row>
-            <Col id="sidebar-wrapper" className="bg-dark col-sm-2">
-              <Sidebar />
-            </Col>
-            <Col id="page-content-wrapper" className="bg-white">
-            <CardAula/>
-            </Col>
+          <Col id="sidebar-wrapper" className="bg-dark col-sm-2">
+            <Sidebar />
+          </Col>
+          <Col id="page-content-wrapper" className="bg-white">
+            <CardAula />
+          </Col>
         </Row>
-    </Container>
+      </Container>
     </>
   );
 }
